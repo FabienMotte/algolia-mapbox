6a622bc47a74a4d3d6237c4a747551f88d936153
Fabien Motte <contact@fabienmotte.com>
2021-02-14 16:23:42 +0100

    Remove CSP rule

--- tasks/server/server.ts
+++ tasks/server/server.ts
@@ -21,7 +21,6 @@ app.use(
         ],
         'img-src': ["'self'", 'data:', 'https://*.unsplash.com'],
         'worker-src': ['blob:'],
-        'require-trusted-types-for': ["'script'"],
       },
     },
   }),
